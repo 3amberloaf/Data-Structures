b63bdcd16bcdf4e66f965e998e1cb494d18fe24f
Amber Sautner <ambersautner3@gmail.com>
2024-03-15 17:33:41 -0400

    rec ursive binary search

---
+++ Algorithms/recursive_bst.py
@@ -0,0 +1,35 @@
+# Binary search using recursion
+
+def binary_search(array, low, high,x):
+    # returns index of x in array if present
+    
+    if high >= low:
+        # checks base case
+        
+        mid = (low + high) // 2
+        
+        if array[mid] == x:
+            return mid
+        # if the element is present in the middle return the middle
+        
+        elif array[mid] > x:
+            return binary_search(array, low, mid - 1, x)
+        # if element is smaller than middle element then rerun algorithm on left subarray
+        
+        else:
+            return binary_search(array, mid + 1, high, x)
+        # otherwise element must be in the right subarray
+    
+    else:
+        return -1
+    # element isnt in the array
+
+array = 2, 4, 5, 5, 5, 8, 10, 20, 25
+x = 2
+
+result = binary_search(array, 0, len(array) - 1, x)
+
+if result != -1:
+    print("element present at index", str(result))
+else:
+    print("Element not in array")
