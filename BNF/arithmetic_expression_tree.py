b6b4321b24e6dbc6b3dbad3ebbc26a9695b62df3
Amber Sautner <ambersautner3@gmail.com>
2024-03-23 11:08:58 -0400

    arithmetic expression tree

--- BNF/arithmetic_expression_tree.py
+++ BNF/arithmetic_expression_tree.py
@@ -1,25 +1,25 @@
-class Lexer:
-    """ Breaks down strings into tokens """
-    def __init__(self, text):
-        self.text = text.replace(' ', '')  # Remove whitespace
+class tokenizer:
+    """ Superclass to break down strings into tokens """
+    def __init__(self, string):
+        self.string = string.replace(' ', '')  # Remove whitespace
         self.index = 0  
         self.current_token = None  
-        self.scan_token()  # Scan tokens immediately
+        self.scan_token()
 
     def scan_token(self):
-        """ Updates token attribute"""
-        if self.index >= len(self.text): # Reach end of index and reset token to none
+        """ Updates token variable """
+        if self.index >= len(self.string): # Reach end of index and reset token to none
             self.current_token = None
             return
         
-        character= self.text[self.index] 
+        character = self.string[self.index] # Token will analyze characters in string
         
-        """ If character is a digit, token reads all the digits"""
+        """ If character is a digit, token reads all the digits before moving on"""
         if character.isdigit():
             start_index = self.index
-            while self.index < len(self.text) and self.text[self.index].isdigit():
+            while self.index < len(self.string) and self.string[self.index].isdigit(): # while there are more characters that are digits to be processed  
                 self.index += 1
-            self.current_token = self.text[start_index:self.index]
+            self.current_token = self.string[start_index:self.index]
         else:
             """ If not a digit, move past """
             self.current_token = character
@@ -32,48 +32,52 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def parse_expression(lexer):
-    """Parse an expression that involves addition or subtraction."""
-    node = parse_factor(lexer)
+def parse_expression(tokenizer):
+    """Parse an expression that involves multipication or division."""
+    """ <expression>  ::=  <factor>  * <expression>   |   <factor>  /  <expression>   |   <factor> """
+    node = parse_factor(tokenizer)
 
-    while lexer.current_token in ("*", "/"): 
-        operator = lexer.current_token
-        lexer.scan_token()
-        right = parse_factor(lexer)
+    while tokenizer.current_token in ("*", "/"): 
+        operator = tokenizer.current_token
+        tokenizer.scan_token()
+        right = parse_factor(tokenizer)
         node = TreeNode(operator, node, right)
 
     return node
 
-def parse_factor(lexer):
-    """Parse a term that involves multiplication or division."""
-    node = parse_term(lexer)
+def parse_factor(tokenizer):
+    """Parse a term that involves addition or subtraction."""
+    """ <factor>  :==  <term> + <factor>  |  <term> - <factor>  |  <term> """
+    node = parse_term(tokenizer)
 
-    while lexer.current_token in ("+", "-"):
-        operator = lexer.current_token
-        lexer.scan_token()
-        right = parse_term(lexer)
+    while tokenizer.current_token in ("+", "-"):
+        operator = tokenizer.current_token
+        tokenizer.scan_token()
+        right = parse_term(tokenizer)
         node = TreeNode(operator, node, right)
 
     return node
 
-def parse_term(lexer):
-    """Parse factors"""
-    if lexer.current_token == "{":
-        lexer.scan_token()  # Skip '('
-        node = parse_expression(lexer)
-        if lexer.current_token != "}":
+def parse_term(tokenizer):
+    """Parse terms"""
+    """ <term>  ::=  {  <expression>  }  |  <literal> """
+    
+    if tokenizer.current_token == "{":
+        tokenizer.scan_token()  # Skip '{'
+        node = parse_expression(tokenizer)
+        if tokenizer.current_token != "}":
             raise Exception("Expected '}'")
-        lexer.scan_token()  # Skip ')'
+        tokenizer.scan_token()  # Skip '}'
     else:
-        if not lexer.current_token.isdigit():
+        if not tokenizer.current_token.isdigit():
             raise Exception("Expected number")
-        node = TreeNode(lexer.current_token)
-        lexer.scan_token()
+        node = TreeNode(tokenizer.current_token)
+        tokenizer.scan_token()
 
     return node
 
 def evaluate_tree(node):
-    """Evaluate the arithmetic expression represented by the tree."""
+    """ Evaluate the arithmetic expression """
     if node.value.isdigit():
         return int(node.value)
     elif node.value == "+":
@@ -94,10 +98,9 @@ def print_tree(node, prefix=""):
 
 ### Execute ###
 
-expression = Lexer('{4+2}*{3-1}')
+expression = tokenizer('{445+21}*{331-1}/91')
 expression_tree = parse_expression(expression)
 result = evaluate_tree(expression_tree)
 print("Result of Evaluation:", result)
-
 print("\nExpression Tree:")
 print_tree(expression_tree)
